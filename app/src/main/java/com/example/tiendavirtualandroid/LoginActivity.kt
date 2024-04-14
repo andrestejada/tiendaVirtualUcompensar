a1a7bf15d498a0bab6e6983bf514d1904f963143
andrestejada <aneduardotejada@gmail.com>
2024-04-14 10:54:41 -0500

    estrcutura basica

---
+++ app/src/main/java/com/example/tiendavirtualandroid/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tiendavirtualandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnCrearCuenta = findViewById<Button>(R.id.btnCrearCuenta)
+
+        btnCrearCuenta.setOnClickListener {
+            val intent = Intent(this,RegistrarActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tiendavirtualandroid/Producto.kt
@@ -0,0 +1,8 @@
+package com.example.tiendavirtualandroid
+
+data class Producto(
+    val imagen: Int, // ID del recurso drawable
+    val titulo: String,
+    val descripcion: String,
+    val precio: Double
+)
